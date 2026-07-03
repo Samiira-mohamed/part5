cf7927c4256d2e8617a5f79d0e13eace238fac6b
Student <student@sirafrica.org>
2026-07-03 14:50:40 +0300

    5.10: sort blogs by number of likes

--- src/App.jsx
+++ src/App.jsx
@@ -131,7 +131,7 @@ const App = () => {
         <BlogForm createBlog={handleCreateBlog} />
       </Togglable>
 
-      {blogs.map(blog =>
+      {[...blogs].sort((a, b) => b.likes - a.likes).map(blog =>
         <Blog key={blog.id} blog={blog} handleLike={handleLike} />
       )}
     </div>
